7c1944f8d994d387e86f7833e9508e1cd77c1029
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-01-31 02:15:46 +0100

    [FIX] Wait for token first

--- client/client.go
+++ client/client.go
@@ -16,27 +16,25 @@ type Client struct {
 
 func (c *Client) Start() {
 	c.active = true
+	done := make(chan bool)
 	go func() {
 		for c.active {
-			if c.Token.Expiry.Before(time.Now()) {
-				req := model.AuthenticateRequest{
-					APIKey: c.APIKey,
-				}
-				resp, err := handler.Authenticate(req)
-				if err != nil {
-					log.Printf("1-authentication-failed: %s", resp.ReturnValue)
-				}
-				if resp.Status != "OK" {
-					log.Printf("2-authentication-failed: %s", resp.ReturnValue)
-				}
-				c.Token = model.Token{
-					Value:  resp.ReturnValue,
-					Expiry: time.Now().Add(10 * time.Minute),
-				}
+			req := model.AuthenticateRequest{
+				APIKey: c.APIKey,
 			}
+			resp, err := handler.Authenticate(req)
+			if err != nil || resp.Status != "OK" {
+				log.Printf("token-renewal-failed: %s", resp.ReturnValue)
+			}
+			c.Token = model.Token{
+				Value:  resp.ReturnValue,
+				Expiry: time.Now().Add(10 * time.Minute),
+			}
+			done <- true
 			time.Sleep(9 * time.Minute)
 		}
 	}()
+	<-done
 }
 
 func (c *Client) Stop() {
