6c8804469a0f32e95b753d45821e8166ba1fe511
Simon Lukas ~ Znox / Plänkler <60503970+Plaenkler@users.noreply.github.com>
2023-02-03 23:52:53 +0100

    [FIX] Datatypes

--- model/file_import.go
+++ model/file_import.go
@@ -3,8 +3,8 @@ package model
 type FileImportRequest struct {
 	File      []byte `json:"file"`
 	FileType  string `json:"fileType,omitempty"`
-	DataType  byte   `json:"dataType,omitempty"`
-	MediaType byte   `json:"mediaType,omitempty"`
-	FormatID  int    `json:"formatId,omitempty"`
+	DataType  string `json:"dataType,omitempty"`
+	MediaType string `json:"mediaType,omitempty"`
+	FormatID  string `json:"formatId,omitempty"`
 	Encoding  string `json:"encoding,omitempty"`
 }
